6336fe3f2b1374c085b46e9a5814eb0535320761
justaway1 <44018646+justaway1@users.noreply.github.com>
2023-03-25 09:07:44 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <script setup>
 import { RouterView } from "vue-router";
-import Home from "./views/Home.vue";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Nav from "./components/Nav.vue";
-=======
-import "animate.css";
->>>>>>> 97be8fc (Updates)
-=======
-import Nav from "./components/Nav.vue";
->>>>>>> ba57a0c (Navigation is Done!)
 </script>
 
 <template>
